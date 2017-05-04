406facafe044bbbfa832a3ae2f2c62035df5b1f8
yang.guo <gy@fir.im>
2017-05-04 16:48:35 +0800

    fastlane gym build while certificate uploaded

--- run.sh
+++ run.sh
@@ -21,12 +21,13 @@ set_project_and_workspace() {
   # Check workspace definition
   if [ -n "$FLOW_IOS_COMPILE_WORKSPACE" ]; then
     params="$params -workspace '$FLOW_IOS_COMPILE_WORKSPACE'"
+    fastlane_params="--workspace '$FLOW_IOS_COMPILE_WORKSPACE'"
   fi
 
   # Check project definition
   if [ -n "$FLOW_IOS_COMPILE_PROJECT" ]; then
     params="$params -project '$FLOW_IOS_COMPILE_PROJECT'"
-    echo " ===1 $FLOW_IOS_COMPILE_PROJECT"
+    fastlane_params="--project '$FLOW_IOS_COMPILE_PROJECT'"
   fi
 
   # Set default project while workspace or project not defined
@@ -35,11 +36,13 @@ set_project_and_workspace() {
     if [ -n "$xcworkspace" ]; then
       export FLOW_IOS_COMPILE_WORKSPACE=${xcworkspace:3}
       params="$params -workspace '$FLOW_IOS_COMPILE_WORKSPACE'"
+      fastlane_params="--workspace '$FLOW_IOS_COMPILE_WORKSPACE'"
       echo " === flow.ci will use workspace'$FLOW_IOS_COMPILE_WORKSPACE' as build argument ==="
 
     else
       export FLOW_IOS_COMPILE_PROJECT=${xcodeproj:3}
       params="$params -project '$FLOW_IOS_COMPILE_PROJECT'"
+      fastlane_params="--project '$FLOW_IOS_COMPILE_PROJECT'"
       echo " === flow.ci will use project '$FLOW_IOS_COMPILE_PROJECT' as build argument ==="
     fi
   fi
@@ -67,20 +70,19 @@ set_scheme() {
     fi
     export FLOW_IOS_COMPILE_SCHEME=$scheme_name
   fi
+
+  fastlane_params="$fastlane_params --scheme '$FLOW_IOS_COMPILE_SCHEME'"
 }
 
 set_destination() {
-  # Set build destination
-  if [ -n "$FLOW_IOS_CODE_SIGN_IDENTITY" ]; then
-    params="$params -sdk $FLOW_IOS_COMPILE_SDK" 
-  else 
-    params="$params -destination 'platform=iOS Simulator,OS=10.1,name=iPhone 6'"
-  fi
+  # Set build destination 
+  params="$params -destination 'platform=iOS Simulator,name=iPhone 6'"
 }
 
 set_configuration() {
   # Set configuration definition
   if [ -n "$FLOW_IOS_COMPILE_CONFIGURATION" ]; then
+    fastlane_params="$fastlane_params --configuration '$FLOW_IOS_COMPILE_CONFIGURATION'"
     params="$params -configuration '$FLOW_IOS_COMPILE_CONFIGURATION'"
   else
     params="$params -configuration 'Release'"
@@ -90,7 +92,7 @@ set_configuration() {
 
 set_code_identity () {
   # Set code identity definition
-    params="$params CODE_SIGN_IDENTITY='iPhone Distribution'"
+    params="$params CODE_SIGN_IDENTITY=''"
 }
 
 export FLOW_IOS_COMPILE_SDK="iphoneos"
@@ -106,41 +108,7 @@ if [ ! $xcodeproj ]; then
  echo ".xcodeproj not found"
  exit 1
 else
-
   check_shared_scheme
-
-  # Find DevelopmentTeam in xcodeproj
-  TEAM_ARR=($(awk -F '=' '/DevelopmentTeam/ {print $2}' "$xcodeproj"/project.pbxproj))
-  # Replace DevelopmentTeam value to none
-  for s in ${TEAM_ARR[@]}
-  do
-    cmd="/DevelopmentTeam  = ${s}/DevelopmentTeam = \"\";/g"
-    sed -i '' s"$cmd" "$xcodeproj"/project.pbxproj
-  done
-
-  # Find PROVISIONING_PROFILE in xcodeproj
-  PROVISIONING_PROFILE_ARR=($(awk -F '=' '/PROVISIONING_PROFILE/ {print $2}' "$xcodeproj"/project.pbxproj))
-  # Replace PROVISIONING_PROFILE value to none
-  for s in ${PROVISIONING_PROFILE_ARR[@]}
-  do
-   cmd="/PROVISIONING_PROFILE = ${s}/PROVISIONING_PROFILE = \"\";/g"
-   sed -i '' s"$cmd" "$xcodeproj"/project.pbxproj
-  done
-
-  # Find DEVELOPMENT_TEAM in xcodeproj
-  DEVELOPMENT_TEAM_ARR=($(awk -F '=' '/DEVELOPMENT_TEAM/ {print $2}' "$xcodeproj"/project.pbxproj))
-  # Replace DEVELOPMENT_TEAM value to none
-  for s in ${DEVELOPMENT_TEAM_ARR[@]}
-  do
-   cmd="/DEVELOPMENT_TEAM = ${s}/DEVELOPMENT_TEAM = \"\";/g"
-   sed -i '' s"$cmd" "$xcodeproj"/project.pbxproj
-  done
-
-  # Replace ProvisioningStyle 'Automatic' to 'Manual'
-  sed -i '' s'/ProvisioningStyle = Automatic/ProvisioningStyle = Manual/g' "$xcodeproj"/project.pbxproj
-
-  # Replace PROVISIONING_PROFILE_SPECIFIER to none
-  sed -i -e "/PROVISIONING_PROFILE_SPECIFIER/d" "$xcodeproj"/project.pbxproj
 fi
 
 
@@ -157,12 +125,15 @@ set_configuration
 set_code_identity
 
 export FLOW_OUTPUT_DIR=${FLOW_WORKSPACE}/output
-cmd="xcodebuild $params SYMROOT=${FLOW_OUTPUT_DIR} | tee ${FLOW_OUTPUT_DIR}/xcodebuild.log | xcpretty -s"
+
+if [ -n "$FLOW_IOS_CODE_SIGN_IDENTITY" ]; then
+  cmd="fastlane gym $fastlane_params --silent --clean"
+else
+  cmd="xcodebuild $params SYMROOT=${FLOW_OUTPUT_DIR} | tee ${FLOW_OUTPUT_DIR}/xcodebuild.log | xcpretty -s"
+fi
+
 echo $cmd
 eval $cmd
 
 # fix xcodebuild 失败 但 $? 为 0
 grep -o "BUILD SUCCEEDED" ${FLOW_OUTPUT_DIR}/xcodebuild.log &> /dev/null
-
-# report html
-# cat ${FLOW_OUTPUT_DIR}/xcodebuild.log | xcpretty -r  html --output ${FLOW_OUTPUT_DIR}/xcodebuild.html
